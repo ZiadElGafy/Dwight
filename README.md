a3851102e94bbce5d992a710fc1aa22de37be067
ZiadElGafy <ziadelgafy@gmail.com>
2023-12-05 02:55:09 +0200

    Added Web Search Feature

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Dwight
+# Dwight v0.1.0
 
 Your everyday personal assistant
+
+# Dependencies
+
+- python: v3.0
--- main.py
+++ main.py
@@ -1,21 +1,27 @@
+from modules.searchTheWeb import driver as searchTheWebDriver
+
 def get_input_option(number_of_options):
     try:
         option = int(input())
         if option < 0 or option > number_of_options:
             raise Exception()
         
-        return option
+        return option - 1
+    
     except:
         print("Invalid Input")
         return get_input_option(number_of_options)
 
 def main():
     options = ["Search the web", "Prayer times", "Weather forecast", "Schedule a google calendar event", "Schedule a google meet", "Chat GPT"]
-    print("Welcome to Dwight!\nPlease select one of the following options:")
+    print("Welcome, I'm Dwight!\nPlease select one of the following options:")
     for i in range(len(options)):
         print(f"{i + 1}: {options[i]}")
 
-    print(get_input_option((len(options))))
+    op = get_input_option((len(options)))
+
+    if options[op] == "Search the web":
+        searchTheWebDriver()
 
 if __name__ == "__main__":
     main()
